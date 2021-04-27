765cba48a68f715590386623de1279da7b5a494f
Potato <67771312+sonsinger12350@users.noreply.github.com>
2021-04-27 09:49:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -46,16 +46,16 @@ if(isset($_POST['submit']))
         //Server settings      
         $mail->SMTPDebug = 0;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.hostinger.vn';                     //Set the SMTP server to send through
+        $mail->Host       = 'xxxxxxxx';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'info@sieuthimaynonglam.com';                     //SMTP username
-        $mail->Password   = 'A1WHghpM8ay';                               //SMTP password
+        $mail->Username   = 'xxxxxxxx';                     //SMTP username
+        $mail->Password   = 'xxxxxxxx';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;
 		//TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         //Recipients
         
-        $mail->setFrom('info@sieuthimaynonglam.com', 'Siêu thị máy nông lâm');
+        $mail->setFrom('xxxxxx', 'Siêu thị máy nông lâm');
         $mail->addAddress('bfs@gagfa.vca');     //Add a recipient
 
 
